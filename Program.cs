3fb3a1059c99aa3219016e4f4c8df00d6e4dd1d5
Adrian Black <adrian_black@idg.com>
2023-02-08 15:04:46 +0000

    command line line change

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace ProGitForProgrammers
            Console.WriteLine("Another Line");
            Console.WriteLine("Another Another Line");
 
-           Console.WriteLine("Commit");
+           Console.WriteLine("Changed line by Sublime");
            Console.WriteLine("Commit 2");
 
         }
